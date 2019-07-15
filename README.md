ff2b4295506d3c294ffd5f811a15b8496dc4e51b
salaboy <salaboy@gmail.com>
2019-07-15 12:14:47 +0100

    Draft create

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # curly-trees
-Experiments on Cloud Native Statemachines and decision trees
+Experiments on Cloud Native statemachines and decision trees in Kubernetes.
+
+The first goal of the experiment is to create a project which shows the following:
+
+1) How to attach a State machine send event to a REST resource event: For example new Author -> Send event E1 to the state machine
+2) From a state machine modify a REST resource
+3) Verify that the state machine state can be reconstructed based on the events emitted  
+4) Understand how to handle external data as JSON
+
+
+
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateContext;
@@ -19,10 +26,15 @@ import org.springframework.statemachine.config.builders.StateMachineTransitionCo
 import org.springframework.statemachine.listener.StateMachineListener;
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
-import reactor.core.Disposable;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+
 import java.util.EnumSet;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -33,6 +45,7 @@ public class DemoApplication implements CommandLineRunner {
 
     @Autowired
     private StateMachine<States, Events> stateMachine;
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -46,6 +59,131 @@ public class DemoApplication implements CommandLineRunner {
         }).subscribe();
 
     }
+
+}
+
+@Component
+class AuthorCreatedEventListener implements ApplicationListener<AuthorCreatedEvent> {
+    @Override
+    public void onApplicationEvent(AuthorCreatedEvent event) {
+        System.out.println("Received Author Created Event - " + event.getSource());
+    }
+}
+
+@Component
+class AuthorDeletedEventListener implements ApplicationListener<AuthorDeletedEvent> {
+    @Override
+    public void onApplicationEvent(AuthorDeletedEvent event) {
+        System.out.println("Received Author Deleted Event - " + event.getSource());
+    }
+}
+
+@Document
+class Author {
+    @Id
+    private String id;
+    private String name;
+    private String lastname;
+
+    public Author(String id, String name, String lastname) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public Author() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
+
+@Repository
+interface AuthorRepository extends ReactiveMongoRepository<Author, String> {
+
+}
+
+
+@RestController
+@RequestMapping("/authors/")
+class AuthorController {
+    private final AuthorRepository authorRepository;
+    private final ApplicationEventPublisher publisher;
+
+    public AuthorController(AuthorRepository authorRepository, ApplicationEventPublisher publisher) {
+        this.authorRepository = authorRepository;
+        this.publisher = publisher;
+    }
+
+    @GetMapping
+    public Flux<Author> getAuthors() {
+        return authorRepository.findAll();
+    }
+
+    @PostMapping
+    public Mono<Author> create(@RequestBody Author author) {
+        return this.authorRepository
+                .save(author)
+                .doOnSuccess(profile -> this.publisher.publishEvent(new AuthorCreatedEvent(profile)));
+
+    }
+
+    @DeleteMapping("{id}")
+    public Mono<Author> delete(@PathVariable("id") String id) {
+        return this.authorRepository.findById(id)
+                .flatMap(a -> this.authorRepository.deleteById(a.getId()).thenReturn(a)
+                        .doOnSuccess(author -> this.publisher.publishEvent(new AuthorDeletedEvent(author))));
+
+    }
+
+
+}
+
+class AuthorCreatedEvent extends ApplicationEvent {
+
+    public AuthorCreatedEvent(Object source) {
+        super(source);
+    }
+
+
+}
+
+class AuthorDeletedEvent extends ApplicationEvent {
+
+    public AuthorDeletedEvent(Object source) {
+        super(source);
+    }
+
 
 }
 
@@ -117,9 +255,6 @@ class StateMachineConfig
         }
 
     }
-
-
-
 
 
 }
